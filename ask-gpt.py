7150789b3c23c56d6646c674912355129d966da2
Shoeb Ahmed <sashoeb94@gmail.com>
2023-06-26 16:18:40 +0530

    Fix indent for spaces

--- ask-gpt.py
+++ ask-gpt.py
@@ -20,21 +20,19 @@ PERSIST = False
 query = sys.argv[1]
 
 if PERSIST and os.path.exists("persist"):
-  print("Reusing index...\n")
-  vectorstore = Chroma(persist_directory="persist", embedding_function=OpenAIEmbeddings())
-  from langchain.indexes.vectorstore import VectorStoreIndexWrapper
-  index = VectorStoreIndexWrapper(vectorstore=vectorstore)
+    print("Reusing index...\n")
+    vectorstore = Chroma(persist_directory="persist", embedding_function=OpenAIEmbeddings())
+    from langchain.indexes.vectorstore import VectorStoreIndexWrapper
+    index = VectorStoreIndexWrapper(vectorstore=vectorstore)
 else:
-  loader = TextLoader('data.txt')
-  # This code can also import folders, including various filetypes like PDFs using the DirectoryLoader.
-  # loader = DirectoryLoader(".", glob="*.txt")
-  if PERSIST:
-    index = VectorstoreIndexCreator(vectorstore_kwargs={"persist_directory":"persist"}).from_loaders([loader])
-  else:
-    index = VectorstoreIndexCreator().from_loaders([loader])
+    loader = TextLoader('data.txt')
+    if PERSIST:
+        index = VectorstoreIndexCreator(vectorstore_kwargs={"persist_directory":"persist"}).from_loaders([loader])
+    else:
+        index = VectorstoreIndexCreator().from_loaders([loader])
 
 chain = RetrievalQA.from_chain_type(
-  llm=ChatOpenAI(model="gpt-3.5-turbo"),
-  retriever=index.vectorstore.as_retriever(search_kwargs={"k": 1}),
+    llm=ChatOpenAI(model="gpt-3.5-turbo"),
+    retriever=index.vectorstore.as_retriever(search_kwargs={"k": 1}),
 )
 print(chain.run(query))
